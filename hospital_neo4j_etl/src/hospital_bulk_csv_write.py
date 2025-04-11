e6d77cd07d072384f784909f7d962fcc99d0190a
47thommy <47thommy@gmail.com>
2025-04-11 10:17:19 +0300

    Remove unused EXAMPLE_CYPHER_CSV_PATH variable and related question loading logic from hospital_bulk_csv_write.py

--- hospital_neo4j_etl/src/hospital_bulk_csv_write.py
+++ hospital_neo4j_etl/src/hospital_bulk_csv_write.py
@@ -10,7 +10,7 @@ PHYSICIANS_CSV_PATH = os.getenv("PHYSICIANS_CSV_PATH")
 PATIENTS_CSV_PATH = os.getenv("PATIENTS_CSV_PATH")
 VISITS_CSV_PATH = os.getenv("VISITS_CSV_PATH")
 REVIEWS_CSV_PATH = os.getenv("REVIEWS_CSV_PATH")
-EXAMPLE_CYPHER_CSV_PATH = os.getenv("EXAMPLE_CYPHER_CSV_PATH")
+
 
 # Neo4j config
 NEO4J_URI = os.getenv("NEO4J_URI")
@@ -131,18 +131,6 @@ def load_hospital_graph_from_csv() -> None:
                          patient_name: reviews.patient_name,
                          physician_name: reviews.physician_name,
                          hospital_name: reviews.hospital_name
-                        }});
-        """
-        _ = session.run(query, {})
-
-    LOGGER.info("Loading question nodes")
-    with driver.session(database="neo4j") as session:
-        query = f"""
-        LOAD CSV WITH HEADERS
-        FROM '{EXAMPLE_CYPHER_CSV_PATH}' AS questions
-        MERGE (Q:Question {{
-                         question: questions.question,
-                         cypher: questions.cypher
                         }});
         """
         _ = session.run(query, {})
